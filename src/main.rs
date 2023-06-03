f4035c5602d2f6c6780dc0f359336b38317b5c70
AnakinSkywalkeer <gurmanavdeepsinghmahal9@gmail.com>
2023-06-04 01:55:31 +0530

    added some improvements

--- src/main.rs
+++ src/main.rs
@@ -16,20 +16,27 @@ fn view(app: &App,frame: Frame){
         while y<1.0{
            let point_x=lerp(-2.0, 2.0, x);
            let point_y=lerp(-2.0, 2.0, y);
-           let f=is_int_set(Complex::new(point_x,point_y));
-           println!("{point_y}");
-           if f==0
+           let p=is_int_set(Complex::new(point_x,point_y)) as u32;
+           if p==0
            {
                let f:f32=point_x as f32;
                let g:f32=point_y as f32;
                draw.ellipse()
-               .color(BLACK)
+               .color(nannou::color::rgb(71 as u64,66 as u64, 88 as u64))
                .w(4.0)
                .h(4.0)
-               .x_y(f*200.0,g*200.0);
-               println!("Hello world");
+               .x_y(f*300.0,g*300.0);
            }
-            y+=0.001;
+           //else {
+            //    let f:f32=point_x as f32;
+             //  let g:f32=point_y as f32;
+             //  draw.ellipse()
+              // .color(WHITE)
+              // .w(2.0)
+             //  .h(2.0)
+             //  .x_y(f*210.0,g*210.0);
+          // }
+           y+=0.001;
         }
         x+=0.001;
 
@@ -39,7 +46,7 @@ fn view(app: &App,frame: Frame){
 fn is_int_set(c:Complex<f64>)-> i32
 {
     let mut z:Complex<f64>=Complex::new(0.0,0.0); 
-    for i in 0..1000 {
+    for i in 0..4000 {
         z=pow(z,2)+c;
         if z.norm()>16.0{
                 return i;
